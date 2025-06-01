4accf515966dc1c2e2ff1ba76392adcab3cfbff6
VelmorK <wchoquehuancab@unsa.edu.pe>
2025-06-01 00:01:24 -0500

    clase BST con nuevos metodos, clase Node con metodos corregidos

--- Lab04/BST.java
+++ Lab04/BST.java
@@ -1,5 +1,18 @@
 package Lab04;
 
-public class BST<T extends Node<T>> {
+public class BST<T extends Comparable<T>> {
+    private Node<T> root;
+
+    public BST() {
+        this.root = null;
+    }
+
+    public void insert(T data){
+        if (root == null) {
+            root = new Node<>(data);
+        } else {
+            root.insert(data);
+        }
+    }
     
 }
--- Lab04/Node.java
+++ Lab04/Node.java
@@ -1,7 +1,59 @@
 package Lab04;
 
-public class Node<T> {
+public class Node<T extends Comparable<T>> {
     private T data;
-    private Node<T> next;
-    private Node<T> previous;
+    private Node<T> right;
+    private Node<T> left;
+    private Node<T> parent;
+
+    public Node(T data) {
+        this.data = data;
+        this.right = null;
+        this.left = null;
+        this.parent = null;
+    }
+    public T getData() {
+        return data;
+    }
+    public void setData(T data) {
+        this.data = data;
+    }
+    public Node<T> getRight() {
+        return right;
+    }
+    public Node<T> getLeft() {
+        return left;
+    }
+    public Node<T> getParent() {
+        return parent;
+    }
+    public void setParent(Node<T> parent) {
+        this.parent = parent;
+    }
+
+    public void insert(T newData){
+        if(newData.compareTo(this.data) < 0){
+            // Insertar a la izquierda
+            if(this.left == null){
+                left = new Node<>(newData);
+                left.setParent(this);
+            }else{
+                this.left.insert(newData);
+            }
+        } else {
+            // Insertar a la derecha
+            if(this.right == null) {
+                right = new Node<T>(newData);
+            } else {
+                this.right.insert(newData);
+            }
+        }
+    }
+    
+    public int compareTo(T other){
+        return this.data.compareTo(other);
+    }
+    
+    
+
 }
