c87b144a80d83cbc6c0e90600d59cd0e11f0d2f2
VelmorK <wchoquehuancab@unsa.edu.pe>
2025-05-31 22:09:41 -0500

    Clases BST y Node para el Lab 4

---
+++ Lab04/BST.java
@@ -0,0 +1,5 @@
+package Lab04;
+
+public class BST<T extends Node<T>> {
+    
+}
---
+++ Lab04/Node.java
@@ -0,0 +1,7 @@
+package Lab04;
+
+public class Node<T> {
+    private T data;
+    private Node<T> next;
+    private Node<T> previous;
+}
