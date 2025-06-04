2c8698d53f64af261ac26169dfc2c24ee1ed2e33
WilliamLawrence25 <wchoquehuancab@unsa.edu.pe>
2025-06-04 14:16:34 -0500

    MAYUSCULAS

--- Lab04/Main.java
+++ Lab04/Main.java
@@ -3,7 +3,7 @@ package Lab04;
 public class Main {
     
     public static void main(String[] args){
-        String word = "Cartethyia";
+        String word = "CARTETHYIA";
         BST<Integer> bst = new BST<>();
 
         System.out.print("Entered word: ");
